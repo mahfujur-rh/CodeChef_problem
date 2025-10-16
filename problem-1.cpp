08e05cbef5406465c3c1d83dd4da280584d294e8
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-16 21:42:44 +0600

    Create problem-1.cpp

---
+++ problem-1.cpp
@@ -0,0 +1,16 @@
+//https://www.codechef.com/problems/ERROR404
+
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int n;cin>>n;
+    if (n == 404)  // 404-- not found --- other wise found
+
+    {
+        cout<<"NOT FOUND"<<endl;
+    }
+    else
+    cout<<"FOUND"<<endl;
+
+    return 0;
+}
