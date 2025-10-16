3cd52afa4ee39067446e9ff205a530980d6c53c4
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-16 21:44:52 +0600

    Create problem-2.cpp

---
+++ problem-2.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/problems/MINWAGE
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if (x > 11) //--x taki 11 suto thakbey--
+    {
+        cout << "YES" << endl;
+    }
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
