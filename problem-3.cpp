51c47d103ecec6e0feda9f4a887b84eb46108641
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-16 21:54:44 +0600

    Create problem-3.cpp

---
+++ problem-3.cpp
@@ -0,0 +1,12 @@
+//https://www.codechef.com/problems/CWC23QUALIF
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int x;cin>>x;
+    if (x>=12)      // x suto holey--output-- No 
+    {
+        cout<<"Yes"<<endl;
+    }
+    else cout<<"No"<<endl;
+ return 0;
+}
