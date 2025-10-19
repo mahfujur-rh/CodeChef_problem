f8d6272c2a55712a982c496b46eb40f2dec618f8
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-19 22:04:37 +0600

    Create problem-13.cpp

---
+++ problem-13.cpp
@@ -0,0 +1,12 @@
+//https://www.codechef.com/problems/PUZHUNT
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;cin>>n;
+	if(6 <= n && n <= 8){
+	    cout<<"Yes"<<endl;
+	}
+    else cout<<"No"<<endl;
+    return 0;
+}
