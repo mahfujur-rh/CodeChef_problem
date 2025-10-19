79b3475c3009d8ae7715e0c20cf9aadf38d4350d
mahfujur-rh <mahfujurrahman1319@gmail.com>
2025-10-19 21:53:38 +0600

    Create problem-7.cpp

---
+++ problem-7.cpp
@@ -0,0 +1,15 @@
+//https://www.codechef.com/problems/SPCP1
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int w,h;
+	cin>>w>>h;
+	if(w >=60 && h<=130){
+	    cout <<"YES" <<endl;
+	}
+	else 
+	cout<<"NO"<<endl;
+
+}
