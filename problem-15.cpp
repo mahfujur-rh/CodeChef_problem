cae9e6a62a39270b4496615a138bcde2c638e998
Mahfujur Rahman <mahfujurrahman1319@gmail.com>
2025-10-21 20:19:26 +0600

    Create problem-15.cpp

---
+++ problem-15.cpp
@@ -0,0 +1,17 @@
+//https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x ,n;
+	    cin>>x>>n;
+	    int total=x/10;
+	    cout<<total*n<<endl;
+	}
+
+}
+
